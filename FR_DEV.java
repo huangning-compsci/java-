262bbec7ffb5638c4ebfd8448409652a5129eb91
huangningcompsci <haoqu595@gmail.com>
2026-09-03 15:38:27 +0800

    实现了程序输入功能

--- FR_DEV.java
+++ FR_DEV.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class equipment{
     String e_ID;
     String e_name;
@@ -7,16 +9,20 @@ class equipment{
     String e_Install_date;
     String e_parameter;
     String e_Status;
-    static void Fill_info(equipment equ){
-        equ.e_ID="PU-1";
-        equ.e_name="Stronger";
-        equ.e_type="Pumping Unit";
-        equ.e_model="pro";
-        equ.e_wellsite="H23-1";
-        equ.e_Install_date="2024-8-26";
-        equ.e_parameter=null;
-        equ.e_Status="Running";
+    //目前还有id自动填的功能未实现：设备缩写有，编号还没
+    equipment(String e_name,String e_type
+            ,String e_model,String e_wellsite,
+            String e_Install_date,String e_parameter,String e_Status){
+        
+        this.e_Install_date=e_Install_date;
+        this.e_Status=e_Status;
+        this.e_model=e_model;
+        this.e_name=e_name;
+        this.e_parameter=e_parameter;
+        this.e_type=e_type;
+        this.e_wellsite=e_wellsite;
     }
+
 
     static void show_info(equipment e){
         System.out.println("id:"+e.e_ID);
@@ -27,23 +33,23 @@ class equipment{
         System.out.println("Status:"+e.e_Status);    
     }
 
-
-
-    
 }
 
 public class FR_DEV{
     
     public static void main(String[] args){
-        equipment[] Equ=new equipment [2];
-        for (int i=0;i<Equ.length;i++){
-            Equ[i]=new equipment();
-        }
-        for(equipment x:Equ){
-            equipment.Fill_info(x);
+        equipment[] Equ=new equipment[2];
         
-        }
+        //以后要把这个print转移到控制面板上
+        System.out.println("请依次输入：设备名称，类型，型号，所属井场，安装日期，额定参数，设备状态");
+        Scanner sc=new Scanner(System.in);
+        Equ[0]=new equipment(
+        sc.nextLine(),sc.nextLine(),
+        sc.nextLine(),sc.nextLine(),
+        sc.nextLine(),sc.nextLine(),sc.nextLine());
+        sc.close();
         equipment.show_info(Equ[0]);
+        }
         
+                
     }
-}
