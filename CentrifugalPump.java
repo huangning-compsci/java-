1dec799c18bc470b9cf5dc788f14702dad63ee79
huangningcompsci <haoqu595@gmail.com>
2026-09-06 13:02:39 +0800

    增加新的类

---
+++ CentrifugalPump.java
@@ -0,0 +1,29 @@
+package FR;
+
+public class CentrifugalPump extends equipment {
+    double ratedFlow;
+    double ratedHead;
+    double inletPressure;
+    double outletPressure;
+    static int index;
+    static String Name="CP";
+    static final DeviceArray<CentrifugalPump> DEVICES=
+            new DeviceArray<>(new CentrifugalPump[10]);
+    
+
+    public CentrifugalPump(
+            String e_wellsite,
+            String e_Install_date,
+            String e_model
+            ){
+            super(
+                "CentrifugalPump",
+                e_model,
+                e_wellsite,
+                e_Install_date,
+                "ratedFlow,ratedHead,inletPressure,outletpressure"
+                );
+            this.e_ID=Name+'_'+(++index);
+            }
+            
+}
