c72381081aa681654e62ed1d249d2cf1ee4e1d5f
huangningcompsci <haoqu595@gmail.com>
2026-09-06 15:06:40 +0800

    设备类的补充

---
+++ FlowMeter.java
@@ -0,0 +1,28 @@
+package FR;
+
+public class FlowMeter extends equipment{
+    double flowRange;
+    String accuracy;
+    double currentFlow;
+    double totalFlow;
+    static String Name="FM";
+    static int index;
+    static final DeviceArray<FlowMeter> DEVICES =
+        new DeviceArray<>(new FlowMeter[10]);
+
+    public FlowMeter(
+            String e_wellsite,
+            String e_Install_date,
+            String e_model
+            ){
+        super(
+            "FlowMeter",
+            e_model,
+            e_wellsite,
+            e_Install_date,
+            "flowRange,accuracy,currentFlow,totalFlow"
+            );
+
+        this.e_ID=Name+'_'+(++index);
+    }
+}
