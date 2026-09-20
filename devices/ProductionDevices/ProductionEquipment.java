c3928f30255032ddbef254c92c65b3743575a745
huangningcompsci <haoqu595@gmail.com>
2026-09-20 16:10:26 +0800

    新增属性:剩余时间和专用计时器;明确抽象方法:切换档位

--- devices/ProductionDevices/ProductionEquipment.java
+++ devices/ProductionDevices/ProductionEquipment.java
@@ -1,8 +1,11 @@
 package devices.ProductionDevices;
 
 import devices.Equipment;
+import devices.RunTimer;
 
 public abstract class ProductionEquipment extends Equipment {
+    protected int runTimeRemain=600;
+    protected RunTimer rt=new RunTimer();
     protected ProductionEquipment(
             String e_ID,
             String e_type,
@@ -12,4 +15,9 @@ public abstract class ProductionEquipment extends Equipment {
             String e_parameter) {
         super(e_ID, e_type, e_model, e_wellsite, e_Install_date, e_parameter);
     }
+    
+
+    
+    abstract boolean Switch(String status);//提供更换档位的功能
+
 }
