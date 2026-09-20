fed46969a50dc37d85ae8ce49abf6b7c547631a3
huangningcompsci <haoqu595@gmail.com>
2026-09-20 16:14:02 +0800

    新增工具类:计时器

---
+++ devices/RunTimer.java
@@ -0,0 +1,39 @@
+package devices;
+
+//本想写成生产力机器类的方法,但是想了想这好像不是多态该做的事,于是抽离出来成为独立类
+public class RunTimer {
+    private long startTime;
+    private long totalTime;
+    private boolean running;
+
+    public boolean start(){
+        if(!running){
+            startTime=System.nanoTime();
+            running=true;
+            return true;
+        }else{
+            System.out.println("请勿重复开启计时");
+            return false;
+        }
+    }
+
+    public boolean stop(){
+        if(running){
+            totalTime+=System.nanoTime()-startTime;
+            running=false;
+            return true;
+        }else{
+            System.out.println("无法关闭,计时器不在计时");
+            return false;
+        }
+    }
+
+    public long getTime(){
+        long time=totalTime;
+        if(running){
+            time+=System.nanoTime()-startTime;
+            startTime=System.nanoTime();
+        }
+        return time/1_000_000_000;
+    }
+}
