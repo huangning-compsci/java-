35005f26018c18e4fa92499bbb2df84d386d3b02
huangningcompsci <haoqu595@gmail.com>
2026-09-20 16:14:17 +0800

    删除了空行

--- devices/FluidTransportDevices/CentrifugalPump.java
+++ devices/FluidTransportDevices/CentrifugalPump.java
@@ -42,6 +42,10 @@ public class CentrifugalPump extends FluidTransportEquipment {
         return DEVICES;
     }
 
+    
+    
+    
+    
     public double getRatedFlow(){
         return ratedFlow;
     }
